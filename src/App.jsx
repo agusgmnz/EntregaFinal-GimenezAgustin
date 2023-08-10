65465ee5295a0510562e2a2778d608e044daf055
agustin <agustingimenez815@gmail.com>
2023-08-10 10:47:06 -0300

    cree el navBar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponent from './components/NavBarComponent'
+
+
+
+const App =() => {
+  
+
+  return (
+    <div>
+      <NavBarComponent/>
+    </div>
+  )
+}
+
+export default App
